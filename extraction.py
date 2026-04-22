937349b9a4d142444e39c0c9d35b41f97683721e
yash-gupta-7 <yash0007gupta@gmail.com>
2026-04-22 11:51:24 +0530

    Initial commit - PDF extraction and processing pipeline

---
+++ extraction.py
@@ -0,0 +1,12 @@
+import pymupdf
+
+file_path = input("Enter PDF file path: ")
+
+doc = pymupdf.open(file_path)
+all_text = ""
+
+for page in doc:
+    all_text += page.get_text() + "\n"
+
+with open("pdf_text.txt", "w") as file:
+    file.write(f"{all_text}")
